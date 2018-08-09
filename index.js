17e6c9399dc1db07d4ee8b46bf4dd1427e7e531b
Ojo Oluwaseun <oluwaseun.ojo@metricinternet.com>
2018-08-09 16:21:38 +0100

    APIs

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import config from './config/database'
 import users from './routes/users';
 
 import University from './models/university';
-import Career from './models/course';
+import Career from './models/career';
 import Course from './models/course';
 import adminusers from './routes/admin-users';
 
@@ -71,14 +71,20 @@ router.route('/university/add').post((req, res) => {
             console.log(university)
         })
         .catch(err => {
-            res.status(400).send('Failed to create new record')
+            res.status(400).send("Failed to add university")
+            console.log(err.message)
         });
 });
 
 router.route('/university/delete/:id').get((req, res) => {
     University.findByIdAndRemove({_id: req.params.id}, (err, university) => {
-        if (err)
-            res.json(err);
+        if (!university){
+            res.status(400).send('oops! university not found')
+        }
+        else if (err){
+            res.status(400).send('Failed to remove university')
+            console.log(err.message);
+        }
         else
             res.json('Removed Successfully');
     })
@@ -86,8 +92,9 @@ router.route('/university/delete/:id').get((req, res) => {
 
 router.route('/university').get((req, res) => {
     University.find((err, university) => {
-        if (err)
-            console.log(err);
+        if (err){
+            res.status(400).send("Failed to get universities")
+            console.log(err.message)}
         else
             res.json(university);
     });
@@ -95,12 +102,199 @@ router.route('/university').get((req, res) => {
 
 router.route('/university/:id').get((req, res) => {
     University.findById(req.params.id, (err, university) => {
-        if (err)
-            console.log(err);
+        if(!university){
+            res.status(400).send('oops! university not found')
+        }
+        else if (err){
+            res.status(400).send('Error finding university')
+            console.log(err.message)}
         else
             res.json(university);
     });
 });
+
+
+//Career APIs
+router.route('/career/add').post((req, res) => {
+    let career = new Career(req.body);
+    career.save()
+        .then(career => {
+            res.status(200).json({'career': 'Added successfully'})
+            console.log(career)
+        })
+        .catch(err => {
+            console.log(err.message);
+            res.status(400).send("Failed to add career")
+        });
+        
+});
+
+router.route('/career/delete/:id').get((req, res) => {
+    Career.findByIdAndRemove({_id: req.params.id}, (err, career) => {
+        if(!career){
+            res.status(400).send('oops! career not found')
+        }
+        else if (err){
+            console.log(err);
+            res.status(400).send("Failed to remove career")}
+        else
+            res.json('Career Removed Successfully');
+    })
+})
+
+router.route('/career').get((req, res) => {
+    Career.find((err, career) => {
+        if (err){
+            res.status(400).send('Failed to get careers')
+            console.log(err);
+        }
+        else
+            res.json(career);
+    });
+});
+
+router.route('/career/:id').get((req, res) => {
+    Career.findById(req.params.id, (err, career) => {
+        if(!career){
+            res.status(400).send('oops! career not found')
+        }
+        else if(err){
+            res.status(400).send("Failed to get career")
+            console.log(err.message);}
+        else
+            console.log("Career found!")
+            res.json(career);
+    });
+});
+
+router.route('/career/update/:id').post((req, res) => {
+    Career.findById(req.params.id)
+    .select("-skills")
+    .exec(function(err, career){
+        if(!career){
+            res.status(400).send('oops! career not found')}
+        else {
+            career.name = req.body.name;
+
+            career.save()
+            .then(career => {
+                res.json('Update done');
+            })
+            .catch(err => {
+                res.status(400).send('Update failed')
+                console.log(err.message);
+            });
+        }
+    });
+})
+
+//Skill APIs
+//Add skill to career
+router.route('/career/:careerId/skill/add').post((req, res) => {
+    Career.findById(req.params.careerId, (err, career) => {
+        if(!career){
+            res.status(400).send('oops! career not found')
+        }
+        if (req.body) {
+           career.skills.push(req.body)}
+        //career.markModified('skills')
+        career.save()
+        .then(career => {
+            res.status(200).json({'skill': 'Added successfully'})
+            console.log(req.body)
+        })
+        .catch(err => {
+            res.status(400).send('Failed to add skill')
+            console.log(err.message)
+        });
+        
+    })
+
+});
+
+//get all skills by career
+router.route('/career/:careerId/skill').get((req, res) => {
+    Career.findById(req.params.careerId, (err, career) => {
+        if(!career){
+           res.status(400).send('oops! career not found')
+        }
+        else if (err){
+           res.status(400).send('Failed to get skills')
+           console.log(err);
+        }
+        else
+            res.json(career.skills);
+    });
+});
+
+//get a specific skill by career
+router.route('/career/:careerId/skill/:skillId').get((req, res) => {
+    Career.findById(req.params.careerId, (err, career) => {
+        if(!career){
+            res.status(400).send('oops! career not found')
+        }
+        var skill = career.skills.id(req.params.skillId)
+        if(!skill){
+            res.status(400).send('oops! skill not found')
+        }
+        else if (err){
+            res.status(400).send('Failed to get skill')
+            console.log(err);
+        }
+        else
+            res.json(skill);
+    });
+});
+
+//update a specific skill in a specific career
+router.route('/career/:careerId/skill/update/:skillId').post((req, res) => {
+    Career.findById(req.params.careerId, (err, career) => {
+        if(!career){
+            res.status(400).send('oops! career not found')}
+        else {
+            var skill = career.skills.id(req.params.skillId)
+            if(!skill){
+                res.status(400).send('oops! skill not found')}
+            else{
+                skill.name = req.body.name;}
+            
+            career.save()
+            .then(career => {
+                res.json('Update done');
+            })
+            .catch(err => {
+                res.status(400).send('Update failed');
+                console.log(err)
+            });
+        }
+    });
+})
+
+//delete a specific skill from a specific career
+router.route('/career/:careerId/skill/delete/:skillId').get((req, res) => {
+   Career.findById(req.params.careerId, (err, career) => {
+        if(!career){
+            res.status(400).send('oops! career not found')}
+        else {
+            var skill = career.skills.id(req.params.skillId)
+            if(!skill){
+                res.status(400).send('oops! skill not found')}
+            else{
+                career.skills.id(req.params.skillId).remove()
+                career.save()
+                .then(career => {
+                    res.json('skill deleted successfully');
+                })
+                .catch(err => {
+                    res.status(400).send('Delete failed')
+                    console.log(err)
+                });
+            }
+        };
+   })
+
+})
+
 
 
 //Course APIs
@@ -112,24 +306,29 @@ router.route('/course/add').post((req, res) => {
             console.log(course)
         })
         .catch(err => {
-            res.status(400).send('Failed to create new record')
+            res.status(400).send('Failed to create course')
+            console.log(err)
         });
-        
 });
 
 router.route('/course/delete/:id').get((req, res) => {
     Course.findByIdAndRemove({_id: req.params.id}, (err, course) => {
-        if (err)
-            res.json(err);
+        if (!course){
+            res.status(400).send('oops! course not found')
+        }
+        else if (err){
+            res.status(400).send('Failed to remove course')
+            console.log(err)}
         else
-            res.json('Removed Successfully');
+            res.json('Course Removed Successfully');
     })
 })
 
 router.route('/course').get((req, res) => {
     Course.find((err, course) => {
-        if (err)
-            console.log(err);
+        if (err){
+            res.status(400).send('Failed to get courses')
+            console.log(err)}
         else
             res.json(course);
     });
@@ -137,120 +336,14 @@ router.route('/course').get((req, res) => {
 
 router.route('/course/:id').get((req, res) => {
     Course.findById(req.params.id, (err, course) => {
-        if(!course){
-            res.statusCode = 404
-            res.statusMessage = "Course not found!"
+        if (!course){
+            res.status(400).send('oops! course not found')
         }
-        else if(err){
-            console.log("Error finding course!")
-            res.statusCode = 500
-            res.statusMessage = err
-        }
+        else if (err){
+            res.status(400).send('Failed to get course')
+            console.log(err)}
         else
-            console.log("Course found!")
             res.json(course);
-    });
-});
-
-//Career API
-//Add career to course
-router.route('/course/:courseId/career/add').post((req, res) => {
-    Course.findById(req.params.courseId, (err, course) => {
-        if (req.body) {
-           course.careers.push(req.body)}
-        course.markModified('careers')
-        course.save()
-        .then(course => {
-            res.status(200).json({'career': 'Added successfully'})
-            console.log(course)
-        })
-        .catch(err => {
-            res.status(400).send('Failed to add career')
-        });
-        
-    })
-
-});
-
-//get all careers by course
-router.route('/course/:courseId/careers').get((req, res) => {
-    Course.findById(req.params.courseId, (err, course) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(course.careers);
-    });
-});
-
-//get a specific career by course
-router.route('/course/:courseId/careers/:careerId').get((req, res) => {
-    Course.findById(req.params.courseId, (err, course) => {
-        var career = course.careers.id(req.params.careerId)
-        if (err)
-            console.log(err);
-        else
-            res.json(career);
-    });
-});
-
-//delete a specific career from a specific course
-router.route('course/:courseId/careers/delete/:careerId').get((req, res) => {
-    Course.findById(req.params.courseId, (err, course) => {
-    //course.careers.findByIdAndRemove({_id: req.params.careerId}, (err, career) => {
-    //   if (err)
-    //        res.json(err);
-    //    else
-    //        res.json('Removed Successfully');
-    //})
-    //})
-    // Equivalent to `parent.children.pull(_id)`
-    console.log(course)
-    course.careers.pull(_id)
-    course.save(function (err) {
-        if (err) res.json(err);
-        console.log('career were removed')
-    })
-});
-})
-
-
-
-//Skill APIs
-router.route('/skill/add').post((req, res) => {
-    let skill = new Skill(req.body);
-    skill.save()
-        .then(skill => {
-            res.status(200).json({'skill': 'Added successfully'})
-            console.log(skill)
-        })
-        .catch(err => {
-            res.status(400).send('Failed to create new record')
-        });
-});
-router.route('/skill/delete/:id').get((req, res) => {
-    Skill.findByIdAndRemove({_id: req.params.id}, (err, skill) => {
-        if (err)
-            res.json(err);
-        else
-            res.json('Removed Successfully');
-    })
-})
-
-router.route('/skill').get((req, res) => {
-    Skill.find((err, skill) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(skill);
-    });
-});
-
-router.route('/skill/:id').get((req, res) => {
-    Skill.findById(req.params.id, (err, skill) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(skill);
     });
 });
 
--- models/course.js
+++ models/course.js
@@ -5,25 +5,10 @@ import config from '../config/database';
 var uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
 
-//Skill Schema
-let skillSchema = new Schema({
-    name: { type: String, required: true, unique: true},
-    
-});
-
-//Career Schema
-let careerSchema = new Schema({
-    name: { type: String, required: true, unique: true},
-    skills: [skillSchema]
-    
-});
-careerSchema.plugin(uniqueValidator, { message: 'Error, expected to be unique.'});
-module.exports = mongoose.model('Career', careerSchema);
-
 //Course Schema
 let courseSchema = new Schema({
     name: { type: String, required: true, unique: true },
-    careers: [careerSchema]
     
 });
+courseSchema.plugin(uniqueValidator, { message: 'Error, expected to be unique.'})
 module.exports = mongoose.model('Course', courseSchema);
